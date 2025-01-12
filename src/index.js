6c31116859097058587c92ce7f4ad18d605fc7a9
Priccila <priccilalucem@gmail.com>
2025-01-12 13:17:13 -0300

    feat/ allowing cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import routes from './routes/routes.js'
 import pool from "./config/db/connection.js";
+import cors from 'cors'; 
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 
 routes(app)
 

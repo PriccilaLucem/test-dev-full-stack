5a01853751c0aed059ccf8c2eedec98867eb1000
Priccila <priccilalucem@gmail.com>
2025-01-13 09:58:38 -0300

    changes in package json

--- src/middlewares/errorHandlerMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const notFoundHandler = (req, res, next) => {
-    res.status(404).json({ error: "Rota não encontrada" });
-  };
-  
-  export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Ocorreu um erro no servidor" });
-  };
-  
--- src/tasks/schema.js
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-const taskSchema = Yup.object({
-  title: Yup.string().required("Título é obrigatório"),
-  description: Yup.string().nullable(),
-  status: Yup.string().oneOf(["PENDING", "IN_PROGRESS", "COMPLETED"], "Status inválido").required("Status é obrigatório")
-});
-
-export default taskSchema;
